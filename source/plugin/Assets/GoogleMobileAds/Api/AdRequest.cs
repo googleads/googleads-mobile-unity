bec2c992d8404f2a2781eca2a2c4010d3173b38c
Ram Parameswaran <rampara@google.com>
2017-04-04 21:29:04 -0700

    Version 3.4.0 of the Google Mobile Ads Unity Plugin

--- source/plugin/Assets/GoogleMobileAds/Api/AdRequest.cs
+++ source/plugin/Assets/GoogleMobileAds/Api/AdRequest.cs
@@ -22,7 +22,7 @@ namespace GoogleMobileAds.Api
 {
     public class AdRequest
     {
-        public const string Version = "3.3.0";
+        public const string Version = "3.4.0";
         public const string TestDeviceSimulator = "SIMULATOR";
 
         private AdRequest(Builder builder)
