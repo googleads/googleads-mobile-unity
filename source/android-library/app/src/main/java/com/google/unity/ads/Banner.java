08602b2f22ffc5697e641ef268fb5cd16f8d461f
Ram Parameswaran <rampara@google.com>
2018-04-13 16:58:33 -0700

    Version 3.13.1 of the Google Mobile Ads Unity plugin

--- source/android-library/app/src/main/java/com/google/unity/ads/Banner.java
+++ source/android-library/app/src/main/java/com/google/unity/ads/Banner.java
@@ -18,10 +18,12 @@ package com.google.unity.ads;
 import android.app.Activity;
 import android.graphics.Color;
 import android.graphics.Point;
+import android.os.Build;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
+import android.view.ViewTreeObserver;
 import android.view.WindowManager;
 import android.widget.PopupWindow;
 
@@ -89,6 +91,12 @@ public class Banner {
      * banner ads as required.
      */
     private View.OnLayoutChangeListener mLayoutChangeListener;
+
+    /**
+     * A {@code ViewTreeObserver.OnGlobalLayoutListener} used to detect if a full screen ad was
+     * shown while a banner ad was on screen.
+     */
+    private ViewTreeObserver.OnGlobalLayoutListener mViewTreeLayoutChangeListener;
 
 
     /**
@@ -204,6 +212,16 @@ public class Banner {
         };
         mUnityPlayerActivity.getWindow().getDecorView().getRootView()
                 .addOnLayoutChangeListener(mLayoutChangeListener);
+
+        // Workaround for issue where ad view will be repositioned to the top of the screen after
+        // a full screen ad is shown on some devices.
+        mViewTreeLayoutChangeListener = new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                updatePosition();
+            }
+        };
+        mAdView.getViewTreeObserver().addOnGlobalLayoutListener(mViewTreeLayoutChangeListener);
     }
 
     private void createPopupWindow() {
@@ -305,6 +323,13 @@ public class Banner {
 
         mUnityPlayerActivity.getWindow().getDecorView().getRootView()
                 .removeOnLayoutChangeListener(mLayoutChangeListener);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            mAdView.getViewTreeObserver()
+                    .removeOnGlobalLayoutListener(mViewTreeLayoutChangeListener);
+        } else {
+            mAdView.getViewTreeObserver().removeGlobalOnLayoutListener(mViewTreeLayoutChangeListener);
+        }
     }
 
     /**
