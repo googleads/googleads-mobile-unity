b7332f43c3b716317096d48ba9dbe87121292fe4
joshuaja <joshuaja.@google.com>
2018-05-31 20:57:54 -0400

    Updated Vungle Unity Mediation Plugin for release version 3.0.0

--- mediation/Vungle/CHANGELOG.md
+++ mediation/Vungle/CHANGELOG.md
@@ -1,5 +1,23 @@
 # Vungle Adapter plugin for Google Mobile Ads SDK for Unity 3D Changelog
 
+## 3.0.0
+
+- Supports Android adapter version 6.2.5.0.
+- Supports iOS adapter version 6.2.0.2.
+- Added the following methods:
+  * `Vungle.UpdateConsentStatus()`  method to set the consent status that will be recorded in the Vungle SDK.
+  * `Vungle.GetCurrentConsentStatus()` method to get the user's current consent status.
+
+## 2.0.0
+
+- Supports Android adapter version 5.3.2.1.
+- Supports iOS adapter version 6.2.0.0.
+
+## 1.1.0
+
+- Supports Android adapter version 5.3.2.1.
+- Supports iOS adapter version 5.4.0.0.
+
 ## 1.0.0
 
 - First release!
---
+++ mediation/Vungle/source/plugin/Assets/GoogleMobileAds/Platforms/iOS/Mediation/Vungle/VungleClient.cs
@@ -0,0 +1,62 @@
+﻿// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#if UNITY_IOS
+
+using UnityEngine;
+
+using GoogleMobileAds.Api.Mediation.Vungle;
+using GoogleMobileAds.Common.Mediation.Vungle;
+
+namespace GoogleMobileAds.iOS.Mediation.Vungle
+{
+    public class VungleClient : IVungleClient
+    {
+        private static VungleClient instance = new VungleClient();
+        private VungleClient() {}
+
+        public static VungleClient Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public void UpdateConsentStatus(VungleConsent consentStatus)
+        {
+            if (consentStatus == VungleConsent.UNKNOWN) {
+                MonoBehaviour.print ("Cannot call '[VungleRouterConsent updateConsentStatus:]' with unknown consent status.");
+                return;
+            }
+
+            string parameterString = "";
+            if (consentStatus == VungleConsent.ACCEPTED) {
+                parameterString = "VungleConsentAccepted";
+            } else if (consentStatus == VungleConsent.DENIED) {
+                parameterString = "VungleConsentDenied";
+            }
+
+            MonoBehaviour.print ("Calling '[VungleRouterConsent updateConsentStatus:]' with argument: " + parameterString);
+            Externs.GADUMUpdateConsentStatus( (int)consentStatus );
+        }
+
+        public VungleConsent GetCurrentConsentStatus()
+        {
+            return (VungleConsent)Externs.GADUMGetCurrentConsentStatus();
+        }
+    }
+}
+
+#endif
