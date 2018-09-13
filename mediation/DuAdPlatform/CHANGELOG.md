20fba23b3336aaa392116a0b2b8b1b067a8f6032
joshuaja <joshuaja@google.com>
2018-09-13 18:01:01 -0400

    Created DU Ad Platform Unity Mediation Plugin for release version 1.0.0

---
+++ mediation/DuAdPlatform/CHANGELOG.md
@@ -0,0 +1,7 @@
+# DU Ad Platform Adapter plugin for Google Mobile Ads SDK for Unity 3D Changelog
+
+## 1.0.0
+- First release!
+- Supports DU Ad Platform Android SDK version 1.1.1.8.
+- Supports DuVideo Android SDK version 1.0.7.2.
+- Supports DU Ad Platform iOS SDK version 1.1.0.
---
+++ mediation/DuAdPlatform/source/plugin/Assets/GoogleMobileAds/Platforms/Android/Mediation/DUAdPlatform/DUAdPlatformClient.cs
@@ -0,0 +1,60 @@
+﻿// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#if UNITY_ANDROID
+
+using UnityEngine;
+using System.Reflection;
+
+using GoogleMobileAds.Common.Mediation.DUAdPlatform;
+
+namespace GoogleMobileAds.Android.Mediation.DUAdPlatform
+{
+    public class DUAdPlatformClient : IDUAdPlatformClient
+    {
+        private static DUAdPlatformClient instance = new DUAdPlatformClient();
+        private DUAdPlatformClient() { }
+
+        public static DUAdPlatformClient Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public void SetConsentStatus(bool consentStatus)
+        {
+            AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            AndroidJavaObject currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+            AndroidJavaClass duAdNetwork = new AndroidJavaClass("com.duapps.ad.base.DuAdNetwork");
+
+            string parameterString = (consentStatus == true ? "true" : "false");
+            MonoBehaviour.print("Calling 'DuAdNetwork.setConsentStatus()' with argument: " + parameterString);
+            duAdNetwork.CallStatic("setConsentStatus", currentActivity, consentStatus);
+        }
+
+        public bool GetConsentStatus()
+        {
+            AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            AndroidJavaObject currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+            AndroidJavaClass duAdNetwork = new AndroidJavaClass("com.duapps.ad.base.DuAdNetwork");
+
+            MonoBehaviour.print("Calling 'DuAdNetwork.getConsentStatus()'");
+            return duAdNetwork.CallStatic<bool>("getConsentStatus", currentActivity);
+        }
+    }
+}
+
+#endif
