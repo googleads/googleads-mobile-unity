560547d7b5142321b3048981f1a9286a53fc3d14
Samuel Stow <samuelstow@google.com>
2019-08-08 15:55:13 -0700

    Update for new release 3.18.3
    PiperOrigin-RevId: 262450873

--- source/plugin/Assets/GoogleMobileAds/Api/AdRequest.cs
+++ source/plugin/Assets/GoogleMobileAds/Api/AdRequest.cs
@@ -21,7 +21,7 @@ namespace GoogleMobileAds.Api
 {
     public class AdRequest
     {
-        public const string Version = "3.18.2";
+        public const string Version = "3.18.3";
         public const string TestDeviceSimulator = "SIMULATOR";
 
         private AdRequest(Builder builder)
