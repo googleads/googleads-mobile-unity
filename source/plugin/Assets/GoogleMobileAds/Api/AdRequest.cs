cdfdb80b84a421564825b9f52e0bcb15a50bd860
Samuel Stow <samuelstow@google.com>
2019-12-29 20:35:32 -0800

    Prepare for release 4.2.0.
    PiperOrigin-RevId: 287504217

--- source/plugin/Assets/GoogleMobileAds/Api/AdRequest.cs
+++ source/plugin/Assets/GoogleMobileAds/Api/AdRequest.cs
@@ -21,7 +21,7 @@ namespace GoogleMobileAds.Api
 {
     public class AdRequest
     {
-        public const string Version = "4.1.0";
+        public const string Version = "4.2.0";
         public const string TestDeviceSimulator = "SIMULATOR";
 
         private AdRequest(Builder builder)
