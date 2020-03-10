5817a9d7c199c64a20515d5ce1ecc51af3be1afe
Mobile Ads Developer Relations <mad-devrel@google.com>
2020-03-09 22:02:04 -0700

    Internal change.
    PiperOrigin-RevId: 300013825

--- source/plugin/Assets/GoogleMobileAds/Api/MobileAds.cs
+++ source/plugin/Assets/GoogleMobileAds/Api/MobileAds.cs
@@ -43,13 +43,7 @@ namespace GoogleMobileAds.Api
 
         public static void Initialize(Action<InitializationStatus> initCompleteAction)
         {
-            client.Initialize((initStatus) => {
-                if (initCompleteAction != null)
-                {
-                    initCompleteAction.Invoke(initStatus);
-                }
-                InitCompleteExecutor.Instance.NotifySdkInitialized();
-            });
+            client.Initialize(initCompleteAction);
             MobileAdsEventExecutor.Initialize();
         }
 
