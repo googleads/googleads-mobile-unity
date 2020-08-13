97251498e164e630c5fbe8f882c0fb9990017f25
Lilly <lnose@google.com>
2020-08-13 18:24:16 -0400

    Added RewardedAd Behaviour

---
+++ source/plugin/Assets/GoogleMobileAds/Platforms/Unity/RewardedAdClient.cs
@@ -0,0 +1,178 @@
+﻿// Copyright (C) 2020 Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Reflection;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GoogleMobileAds;
+using GoogleMobileAds.Api;
+using GoogleMobileAds.Common;
+
+namespace GoogleMobileAds.Unity
+{
+    public class RewardedAdClient : BaseAdDummyClient, IRewardedAdClient
+    {
+        // Ad event fired when the rewarded ad has been received.
+        public event EventHandler<EventArgs> OnAdLoaded;
+        // Ad event fired when the rewarded ad has failed to load.
+        public event EventHandler<AdErrorEventArgs> OnAdFailedToLoad;
+        // Ad event fired when the rewarded ad has failed to show.
+        public event EventHandler<AdErrorEventArgs> OnAdFailedToShow;
+        // Ad event fired when the rewarded ad is opened.
+        public event EventHandler<EventArgs> OnAdOpening;
+        // Ad event fired when the rewarded ad has rewarded the user.
+        public event EventHandler<Reward> OnUserEarnedReward;
+        // Ad event fired when the rewarded ad is closed.
+        public event EventHandler<EventArgs> OnAdClosed;
+        // Ad event fired when the rewarded ad is estimated to have earned money.
+        public event EventHandler<AdValueEventArgs> OnPaidEvent;
+
+        private Dictionary<AdSize, string> prefabAds = new Dictionary<AdSize, string>() {
+            {new AdSize (320,480), "DummyAds/Rewarded/320x480" },
+            {new AdSize (480,320), "DummyAds/Rewarded/480x320"},
+            {new AdSize (768,1024), "DummyAds/Rewarded/768x1024" },
+            {new AdSize (1024,768), "DummyAds/Rewarded/1024x768"}
+        };
+
+        private ButtonBehaviour buttonBehaviour;
+
+        private void AddClickBehavior(GameObject dummyAd)
+        {
+            Image myImage = dummyAd.GetComponentInChildren<Image>();
+            Button button = myImage.GetComponentInChildren<Button>();
+            button.onClick.AddListener(() => {
+                buttonBehaviour.OpenURL();
+            });
+            Button[] innerButtons = dummyAd.GetComponentsInChildren<Button>();
+
+            innerButtons[1].onClick.AddListener(() =>
+            {
+                AdBehaviour.DestroyAd(dummyAd);
+                if (OnAdClosed != null)
+                {
+                    OnAdClosed.Invoke(this, new EventArgs());
+                }
+                AdBehaviour.ResumeGame();
+            });
+        }
+
+        private void CreateButtonBehavior()
+        {
+            buttonBehaviour = new ButtonBehaviour();
+            buttonBehaviour.OnAdOpening += OnAdOpening;
+        }
+
+        // Creates a rewarded ad.
+        public void CreateRewardedAd(string adUnitId)
+        {
+            Debug.Log("Dummy " + MethodBase.GetCurrentMethod().Name);
+            CreateButtonBehavior();
+        }
+
+        // Load a rewarded ad.
+        public void LoadAd(AdRequest request)
+        {
+            Debug.Log("Dummy " + MethodBase.GetCurrentMethod().Name);
+            if (Screen.width > Screen.height) //Landscape
+            {
+                if (Screen.width > 800)
+                {
+                    LoadAndSetPrefabAd(prefabAds[new AdSize(1024, 768)]);
+                }
+                else
+                {
+                    LoadAndSetPrefabAd(prefabAds[new AdSize(480, 320)]);
+                }
+            }
+            else
+            {
+                if (Screen.height < 1080)
+                {
+                    LoadAndSetPrefabAd(prefabAds[new AdSize(320, 480)]);
+                }
+                else
+                {
+                    LoadAndSetPrefabAd(prefabAds[new AdSize(768, 1024)]);
+                }
+            }
+
+
+
+            if (OnAdLoaded != null)
+            {
+                OnAdLoaded.Invoke(this, EventArgs.Empty);
+            }
+            Show();
+        }
+
+        // Determines whether the rewarded ad has loaded.
+        public bool IsLoaded()
+        {
+            Debug.Log("Dummy " + MethodBase.GetCurrentMethod().Name);
+            if (prefabAd != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // Returns the mediation adapter class name.
+        public string MediationAdapterClassName()
+        {
+            return new ResponseInfoDummyClient().GetMediationAdapterClassName();
+        }
+
+        // Returns the reward item for the loaded rewarded ad.
+        public Reward GetRewardItem()
+        {
+            Debug.Log("Dummy " + MethodBase.GetCurrentMethod().Name);
+            return null;
+        }
+
+        // Shows the rewarded ad on the screen.
+        public void Show()
+        {
+            Debug.Log("Dummy " + MethodBase.GetCurrentMethod().Name);
+            if (IsLoaded() == true)
+            {
+                dummyAd = AdBehaviour.ShowAd(prefabAd, new Vector3(0, 0, 1));
+                AdBehaviour.PauseGame();
+                AddClickBehavior(dummyAd);
+                dummyAd.AddComponent<Countdown>(); 
+            }
+            else
+            {
+                Debug.Log("No Ad Loaded");
+            }
+        }
+
+        // Sets the server side verification options
+        public void SetServerSideVerificationOptions(ServerSideVerificationOptions serverSideVerificationOptions)
+        {
+            Debug.Log("Dummy " + MethodBase.GetCurrentMethod().Name);
+        }
+
+        // Returns ad request Response info client.
+        public IResponseInfoClient GetResponseInfoClient()
+        {
+            Debug.Log("Dummy " + MethodBase.GetCurrentMethod().Name);
+            return new ResponseInfoDummyClient();
+        }
+    }
+}
