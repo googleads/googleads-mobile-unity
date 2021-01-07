6b8e4b5111f6d73c9563800bc1cb0a4462b1d088
Samuel Stow <samuelstow@google.com>
2021-01-07 13:22:00 -0800

    Fix erroneous removal of code
    PiperOrigin-RevId: 350625811

--- source/plugin/Assets/GoogleMobileAds/Editor/GoogleMobileAdsSettingsEditor.cs
+++ source/plugin/Assets/GoogleMobileAds/Editor/GoogleMobileAdsSettingsEditor.cs
@@ -12,6 +12,36 @@ namespace GoogleMobileAds.Editor
         public static void OpenInspector()
         {
             Selection.activeObject = GoogleMobileAdsSettings.Instance;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.LabelField("Google Mobile Ads App ID", EditorStyles.boldLabel);
+            EditorGUI.indentLevel++;
+
+            GoogleMobileAdsSettings.Instance.GoogleMobileAdsAndroidAppId =
+                    EditorGUILayout.TextField("Android",
+                            GoogleMobileAdsSettings.Instance.GoogleMobileAdsAndroidAppId);
+
+            GoogleMobileAdsSettings.Instance.GoogleMobileAdsIOSAppId =
+                    EditorGUILayout.TextField("iOS",
+                            GoogleMobileAdsSettings.Instance.GoogleMobileAdsIOSAppId);
+
+            EditorGUILayout.HelpBox(
+                    "Google Mobile  Ads App ID will look similar to this sample ID: ca-app-pub-3940256099942544~3347511713",
+                    MessageType.Info);
+
+            EditorGUI.indentLevel--;
+            EditorGUILayout.Separator();
+
+            EditorGUILayout.LabelField("AdMob-specific settings", EditorStyles.boldLabel);
+            EditorGUI.indentLevel++;
+
+            EditorGUI.BeginChangeCheck();
+
+            GoogleMobileAdsSettings.Instance.DelayAppMeasurementInit =
+                    EditorGUILayout.Toggle(new GUIContent("Delay app measurement"),
+                    GoogleMobileAdsSettings.Instance.DelayAppMeasurementInit);
 
             if (GoogleMobileAdsSettings.Instance.DelayAppMeasurementInit) {
                 EditorGUILayout.HelpBox(
