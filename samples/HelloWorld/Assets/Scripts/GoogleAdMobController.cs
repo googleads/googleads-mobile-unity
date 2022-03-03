1a9ddbb15dd4cb61e9269392c4af913b37209dec
Mobile Ads Developer Relations <mad-devrel@google.com>
2022-03-03 13:52:12 -0800

    Change SmartBanner to Banner in 'HelloWorld' sample
    PiperOrigin-RevId: 432275883

--- samples/HelloWorld/Assets/Scripts/GoogleAdMobController.cs
+++ samples/HelloWorld/Assets/Scripts/GoogleAdMobController.cs
@@ -126,7 +126,7 @@ public class GoogleAdMobController : MonoBehaviour
         }
 
         // Create a 320x50 banner at top of the screen
-        bannerView = new BannerView(adUnitId, AdSize.SmartBanner, AdPosition.Top);
+        bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Top);
 
         // Add Event Handlers
         bannerView.OnAdLoaded += (sender, args) => OnAdLoadedEvent.Invoke();
