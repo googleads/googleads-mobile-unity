49a429e89fa50cca68dcc03135331d8e6c9d9120
Mobile Ads Developer Relations <mad-devrel-github-bot@google.com>
2022-11-30 17:15:49 -0800

    Added OnAdClicked to Unity RewardedAdClient.
    PiperOrigin-RevId: 492060364

--- source/plugin/Assets/GoogleMobileAds/Platforms/Unity/RewardingAdBaseClient.cs
+++ source/plugin/Assets/GoogleMobileAds/Platforms/Unity/RewardingAdBaseClient.cs
@@ -42,6 +42,8 @@ namespace GoogleMobileAds.Unity
         public event EventHandler<EventArgs> OnAdDidDismissFullScreenContent;
         // Ad event fired when an ad impression has been recorded.
         public event EventHandler<EventArgs> OnAdDidRecordImpression;
+        // Ad event fired when an ad impression has been clicked.
+        public event Action OnAdClicked;
 
         internal static readonly Dictionary<AdSize, string> prefabAds = new Dictionary<AdSize, string>()
         {
@@ -59,6 +61,10 @@ namespace GoogleMobileAds.Unity
             button.onClick.AddListener(() =>
             {
                 buttonBehaviour.OpenURL();
+                if (OnAdClicked != null)
+                {
+                    OnAdClicked();
+                }
             });
             Button[] innerButtons = adImage.GetComponentsInChildren<Button>();
 
@@ -145,6 +151,10 @@ namespace GoogleMobileAds.Unity
                 if (OnAdDidPresentFullScreenContent != null)
                 {
                     OnAdDidPresentFullScreenContent.Invoke(this, EventArgs.Empty);
+                }
+                if (OnAdDidRecordImpression != null)
+                {
+                    OnAdDidRecordImpression(this, EventArgs.Empty);
                 }
             }
             else
