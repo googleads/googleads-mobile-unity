24a18984487ea4a93d840a5232af8e58b897c16b
Mobile Ads Developer Relations <mad-devrel-github-bot@google.com>
2022-12-19 23:39:49 -0800

    - API classes for   -  [ConsentInformation](https://developers.google.com/admob/ump/android/api/reference/com/google/android/ump/ConsentInformation)   -  [ConsentForm](https://developers.google.com/admob/ump/android/api/reference/com/google/android/ump/ConsentForm)
    PiperOrigin-RevId: 496580490

---
+++ source/plugin/Assets/GoogleMobileAds/Ump/Api/ConsentForm.cs
@@ -0,0 +1,55 @@
+// Copyright (C) 2022 Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+
+using GoogleMobileAds.Ump.Api;
+using GoogleMobileAds.Ump.Common;
+
+namespace GoogleMobileAds.Ump.Api
+{
+    /// <summary>
+    /// A rendered form for collecting consent from a user.
+    /// </summary>
+    public class ConsentForm
+    {
+        private IConsentFormClient _client;
+
+        internal ConsentForm(IConsentFormClient client)
+        {
+            _client = client;
+        }
+
+        /// <summary>
+        /// Loads a consent form.
+        /// </summary>
+        /// <param name="formLoadCallback">Called when the client loads a consent form or
+        /// returns an error. </param>
+        public static void Load(Action<ConsentForm, FormError> formLoadCallback)
+        {
+            IConsentFormClient client = ConsentInformation.ClientFactory.ConsentFormClient();
+            client.Load(() => formLoadCallback(new ConsentForm(client), null),
+                        error => formLoadCallback(null, error));
+        }
+
+        /// <summary>
+        /// Shows the consent form.
+        /// </summary>
+        /// <param name="onDismissed">Called when the consent form is dismissed. </param>
+        public void Show(Action<FormError> onDismissed)
+        {
+            _client.Show(onDismissed);
+        }
+    }
+}
---
+++ source/plugin/Assets/GoogleMobileAds/Ump/Api/ConsentInformation.cs
@@ -0,0 +1,93 @@
+// Copyright (C) 2022 Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Reflection;
+using UnityEngine;
+
+using GoogleMobileAds.Ump.Common;
+
+namespace GoogleMobileAds.Ump.Api
+{
+    /// <summary>
+    /// Utility methods for managing consent information from users.
+    /// </summary>
+    public class ConsentInformation
+    {
+        internal static IUmpClientFactory ClientFactory
+        {
+            get
+            {
+                if (_clientFactory == null)
+                {
+                    _clientFactory = Utils.GetClientFactory();
+                }
+                return _clientFactory;
+            }
+            set
+            {
+                _clientFactory = value;
+            }
+        }
+        private static IUmpClientFactory _clientFactory;
+
+        /// <summary>
+        /// The user's consent status.
+        /// This value is cached between app sessions and can be read before
+        /// requesting updated parameters.
+        /// </summary>
+        public static ConsentStatus ConsentStatus
+        {
+            get
+            {
+                IConsentInformationClient client = ClientFactory.ConsentInformationClient();
+                return (ConsentStatus)client.GetConsentStatus();
+            }
+        }
+
+        /// <summary>
+        /// Requests consent information update.
+        /// </summary>
+        /// <param name="request">The request params.</param>
+        /// <param name="consentInfoUpdateCallback">Called when client updates ConsentStatus or
+        /// returns a FormError argument with ErrorCode and Message. </param>
+        public static void Update(ConsentRequestParameters request,
+                                  Action<FormError> consentInfoUpdateCallback)
+        {
+            IConsentInformationClient client = ClientFactory.ConsentInformationClient();
+            client.Update(request, () => consentInfoUpdateCallback(null),
+                          error => consentInfoUpdateCallback(error));
+        }
+
+        /// <summary>
+        /// Clears all consent status from persistent storage.
+        /// </summary>
+        public static void Reset()
+        {
+            IConsentInformationClient client = ClientFactory.ConsentInformationClient();
+            client.Reset();
+        }
+
+        /// <summary>
+        /// Returns <c>true</c> if a
+        /// <see cref="GoogleMobileAds.Ump.Api.ConsentForm">ConsentForm</see>
+        /// is available, otherwise returns <c>false</c>.
+        /// </summary>
+        public static bool IsConsentFormAvailable()
+        {
+            IConsentInformationClient client = ClientFactory.ConsentInformationClient();
+            return client.IsConsentFormAvailable();
+        }
+    }
+}
