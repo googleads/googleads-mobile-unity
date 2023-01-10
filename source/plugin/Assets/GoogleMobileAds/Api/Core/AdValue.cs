d9b1e9a07f73065e9c847b7c7363e90845c50d0e
Mobile Ads Developer Relations <mad-devrel-github-bot@google.com>
2023-01-10 13:27:39 -0800

    Updated xml-doc summary for AdValue.cs
    PiperOrigin-RevId: 501079565

--- source/plugin/Assets/GoogleMobileAds/Api/Core/AdValue.cs
+++ source/plugin/Assets/GoogleMobileAds/Api/Core/AdValue.cs
@@ -13,36 +13,50 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System;
-
-using GoogleMobileAds.Api;
-
 namespace GoogleMobileAds.Api
 {
-
-    // The monetary value earned from an ad.
+    /// <summary>
+    /// The monetary value earned from an ad.
+    /// </summary>
     public class AdValue
     {
+        /// <summary>
+        /// The precision of the reported ad value.
+        /// </summary>
         public enum PrecisionType
         {
-            // An ad value with unknown precision.
+            /// <summary>
+            /// An ad value with unknown precision.
+            /// </summary>
             Unknown = 0,
-            // An ad value estimated from aggregated data.
+            /// <summary>
+            /// An ad value estimated from aggregated data.
+            /// </summary>
             Estimated = 1,
-            // A publisher-provided ad value, such as manual CPMs in a mediation group.
+            /// <summary>
+            /// A publisher-provided ad value, such as manual CPMs in a mediation group.
+            /// </summary>
             PublisherProvided = 2,
-            // The precise value paid for this ad.
+            /// <summary>
+            /// The precise value paid for this ad.
+            /// </summary>
             Precise = 3
         }
 
-        // The precision of the reported ad value.
+        /// <summary>
+        /// The precision of the reported ad value.
+        /// </summary>
         public PrecisionType Precision { get; set; }
 
-        // The ad's value in micro-units, where 1,000,000 micro-units equal one unit of
-        // the currency.
+        /// <summary>
+        /// The ad's value in micro-units, where 1,000,000 micro-units equal one unit of
+        /// the currency.
+        /// </summary>
         public long Value { get; set; }
 
-        // The value's ISO 4217 currency code.
+        /// <summary>
+        /// The value's ISO 4217 currency code.
+        /// </summary>
         public string CurrencyCode { get; set; }
     }
 }
