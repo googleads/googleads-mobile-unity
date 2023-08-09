9166d31eddf1f1d0d4c7b036be1a578f4173c3b2
Mobile Ads Developer Relations <mad-devrel-github-bot@google.com>
2023-08-08 17:37:36 -0700

    Update UMP Error in iOS Plugin.
    PiperOrigin-RevId: 554994481

--- source/plugin/Assets/Plugins/Ump/iOS/GADUUMPError.m
+++ source/plugin/Assets/Plugins/Ump/iOS/GADUUMPError.m
@@ -2,9 +2,10 @@
 
 #import "GADUUMPError.h"
 
-NSErrorDomain const UMPErrorDomain = @"com.google.user_messaging_platform.unity_ios_bridge";
+NSErrorDomain const GADUUMPErrorDomain = @"com.google.user_messaging_platform.unity_ios_bridge";
 
-NSError *_Nonnull UMPErrorWithCodeAndDescription(NSInteger code, NSString *_Nonnull description) {
+NSError *_Nonnull GADUUMPErrorWithCodeAndDescription(NSInteger code,
+                                                     NSString *_Nonnull description) {
   return [[NSError alloc]
       initWithDomain:GADUUMPErrorDomain
                 code:code
