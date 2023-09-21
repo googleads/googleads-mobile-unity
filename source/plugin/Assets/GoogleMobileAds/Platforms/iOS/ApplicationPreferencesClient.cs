191afa0a32c595cadf8a79254b8cc7eb10e23b9e
Mobile Ads Developer Relations <mad-devrel-github-bot@google.com>
2023-09-21 16:53:31 -0700

    ApplicationPreferences missing UNITY_IOS define.
    PiperOrigin-RevId: 567457545

--- source/plugin/Assets/GoogleMobileAds/Platforms/iOS/ApplicationPreferencesClient.cs
+++ source/plugin/Assets/GoogleMobileAds/Platforms/iOS/ApplicationPreferencesClient.cs
@@ -1,3 +1,4 @@
+#if UNITY_IOS
 // Copyright (C) 2023 Google LLC.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -43,3 +44,4 @@ namespace GoogleMobileAds.iOS
         }
     }
 }
+#endif
