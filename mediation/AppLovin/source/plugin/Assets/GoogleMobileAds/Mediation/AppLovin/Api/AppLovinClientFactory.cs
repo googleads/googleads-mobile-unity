b5d3e59ba3aa350eea2bdf9070f20f29bf39cb30
Mobile Ads Developer Relations <mad-devrel-noreply@google.com>
2024-06-24 20:14:58 -0700

    Move `AppLovinClientFactory` to the Api assembly.
    PiperOrigin-RevId: 646309845

--- mediation/AppLovin/source/plugin/Assets/GoogleMobileAds/Mediation/AppLovin/Api/AppLovinClientFactory.cs
+++ mediation/AppLovin/source/plugin/Assets/GoogleMobileAds/Mediation/AppLovin/Api/AppLovinClientFactory.cs
@@ -12,9 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using UnityEngine;
-using System.Collections;
-
 using GoogleMobileAds.Mediation.AppLovin.Common;
 
 namespace GoogleMobileAds.Mediation.AppLovin
