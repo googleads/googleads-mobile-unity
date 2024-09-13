f39cf300ab368fd8d6d3ac848a74516da9506109
Mobile Ads Developer Relations <mad-devrel-noreply@google.com>
2024-09-13 16:14:10 -0700

    Remove the redundant Line folder from Api and unused imports from the Line Unity mediation plugin. Use the string alias for System.String consistently.
    PiperOrigin-RevId: 674468971

--- mediation/Line/source/plugin/Assets/GoogleMobileAds/Mediation/Line/Api/LineMediationExtras.cs
+++ mediation/Line/source/plugin/Assets/GoogleMobileAds/Mediation/Line/Api/LineMediationExtras.cs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System;
-
 using GoogleMobileAds.Api.Mediation;
 
 namespace GoogleMobileAds.Mediation.Line.Api
@@ -29,7 +27,7 @@ namespace GoogleMobileAds.Mediation.Line.Api
             get { return "com.google.unity.mediation.line.LineUnityExtrasBuilder"; }
         }
 
-        public override String IOSMediationExtraBuilderClassName
+        public override string IOSMediationExtraBuilderClassName
         {
             get { return "LineExtrasBuilder"; }
         }
