ea221fd92e9074faa36b1fe1973d904e4ed1b37d
Nicholas Ventimiglia <nventimiglia@google.com>
2024-10-24 15:17:30 -0700

    Added Prerelease sample.
    PiperOrigin-RevId: 689533281

---
+++ samples/Prerelease/Assets/Scripts/Utility/SceneMenuList.cs
@@ -0,0 +1,49 @@
+using System;
+using UnityEngine;
+
+namespace GoogleMobileAds.Samples.Utility
+{
+    /// <summary>
+    /// Instantiates a list of Scene menu buttons.
+    /// </summary>
+    [AddComponentMenu("GoogleMobileAds/Samples/Utility/SceneMenuList")]
+    public class SceneMenuList : MonoBehaviour
+    {
+        // Data class for configuring the scene list menu.
+        private struct SceneInfo
+        {
+            public string Text;
+            public string SceneToLoad;
+            public SceneInfo(string text, string sceneToLoad)
+            {
+                Text = text;
+                SceneToLoad = sceneToLoad;
+            }
+        }
+
+        // SceneMenuList Configurations.
+        private readonly SceneInfo[] Scenes = new SceneInfo[]
+        {
+            new SceneInfo("Preload Ads", "GooglePreloadAdsScene"),
+        };
+
+        [Tooltip("Prefab for the SceneMenuButton.")]
+        public GameObject ButtonPrefab;
+
+        [Tooltip("Parent transform for the instantiated menu.")]
+        public Transform MenuContainer;
+
+        protected void Awake()
+        {
+            for (int i = Scenes.Length - 1; i >= 0 ; i--)
+            {
+                var info = Scenes[i];
+                var instance = Instantiate(ButtonPrefab, MenuContainer);
+                var button = instance.GetComponent<SceneMenuButton>();
+                button.SceneToLoadName = info.SceneToLoad;
+                button.Label.text = info.Text;
+                button.transform.SetAsFirstSibling();
+            }
+        }
+    }
+}
