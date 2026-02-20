07ab269305af2b4b26794a172032455d355b77bf
Mobile Ads Developer Relations <mad-devrel-noreply@google.com>
2026-02-20 01:12:44 -0800

    Adds ability to pass ConsentSyncId to UMP SDK.
    PiperOrigin-RevId: 872777890

--- source/plugin/Assets/Plugins/Ump/iOS/GADURequestParameters.h
+++ source/plugin/Assets/Plugins/Ump/iOS/GADURequestParameters.h
@@ -9,6 +9,9 @@
 /// Indicates whether the user is tagged for under age of consent.
 @property(nonatomic) BOOL tagForUnderAgeOfConsent;
 
+/// Consent sync ID for the request.
+@property(nonatomic, nullable) NSString *consentSyncID;
+
 /// Debug settings for the request.
 @property(nonatomic, nullable) GADUDebugSettings *debugSettings;
 
