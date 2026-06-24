4c1ff04ee881bfd041244a4cd08a31e39361c81b
Mobile Ads Developer Relations <mad-devrel-noreply@google.com>
2026-06-24 08:34:11 -0700

    No public description
    PiperOrigin-RevId: 937356782

---
+++ source/plugin/Assets/GoogleMobileAds/Editor/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using System.Runtime.CompilerServices;
+
+// Allow our unified unmocked test assembly to access internal editor members.
+[assembly: InternalsVisibleTo("GoogleMobileAds.Editor.Tests")]
