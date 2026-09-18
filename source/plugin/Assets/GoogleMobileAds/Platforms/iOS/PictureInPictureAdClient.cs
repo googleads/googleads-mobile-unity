0f36570ab276af3b9fbd572ac1acc2aedf0ce7ed
Mobile Ads Developer Relations <mad-devrel-noreply@google.com>
2026-09-18 15:20:34 -0700

    Add iOS externs for Picture-in-Picture ads. Complete iOS client file.
    PiperOrigin-RevId: 984095877

--- source/plugin/Assets/GoogleMobileAds/Platforms/iOS/PictureInPictureAdClient.cs
+++ source/plugin/Assets/GoogleMobileAds/Platforms/iOS/PictureInPictureAdClient.cs
@@ -1,3 +1,4 @@
+#if UNITY_IOS
 // Copyright 2026 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,57 +14,333 @@
 // limitations under the License.
 
 using System;
+using System.Runtime.InteropServices;
 using GoogleMobileAds.Api;
 using GoogleMobileAds.Common;
 
 namespace GoogleMobileAds.iOS
 {
-    public class PictureInPictureAdClient : IPictureInPictureAdClient
+    public class PictureInPictureAdClient :
+        IPictureInPictureAdClient, IDisposable
     {
+        private IntPtr _pictureInPictureAdPtr;
+        private IntPtr _pictureInPictureAdClientPtr;
+
+        #region Picture-in-Picture callback types
+
+        internal delegate void GADUPictureInPictureAdLoadedCallback(
+            IntPtr pipAdClient);
+
+        internal delegate void GADUPictureInPictureAdFailToLoadCallback(
+            IntPtr pipAdClient, IntPtr error);
+
+        internal delegate void GADUPictureInPictureAdShownCallback(
+            IntPtr pipAdClient);
+
+        internal delegate void GADUPictureInPictureAdHiddenCallback(
+            IntPtr pipAdClient);
+
+        internal delegate void
+            GADUPictureInPictureAdDidRecordImpressionCallback(
+                IntPtr pipAdClient);
+
+        internal delegate void GADUPictureInPictureAdDidRecordClickCallback(
+            IntPtr pipAdClient);
+
+        internal delegate void
+            GADUPictureInPictureAdDidFailToShowCallback(
+                IntPtr pipAdClient, IntPtr error);
+
+        internal delegate void
+            GADUPictureInPictureAdWillPresentFullScreenContentCallback(
+                IntPtr pipAdClient);
+
+        internal delegate void
+            GADUPictureInPictureAdDidDismissFullScreenContentCallback(
+                IntPtr pipAdClient);
+
+        internal delegate void GADUPictureInPictureAdPaidEventCallback(
+            IntPtr pipAdClient, int precision, long value,
+            string currencyCode);
+
+        #endregion
+
         public event Action OnAdLoaded;
         public event Action<LoadAdErrorClientEventArgs> OnAdFailedToLoad;
         public event Action OnAdShown;
         public event Action OnAdHidden;
         public event Action OnAdDidRecordImpression;
         public event Action OnAdClicked;
-        public event Action<AdErrorClientEventArgs> OnAdFailedToPresentFullScreenContent;
+        public event Action<AdErrorClientEventArgs>
+            OnAdFailedToPresentFullScreenContent;
         public event Action OnAdDidPresentFullScreenContent;
         public event Action OnAdDidDismissFullScreenContent;
         public event Action<AdValue> OnPaidEvent;
 
+        public PictureInPictureAdClient()
+        {
+            CreatePictureInPictureAd();
+        }
+
+        // This property should be used when setting the _pictureInPictureAdPtr.
+        private IntPtr PictureInPictureAdPtr
+        {
+            get
+            {
+                return this._pictureInPictureAdPtr;
+            }
+            set
+            {
+                Externs.GADURelease(this._pictureInPictureAdPtr);
+                this._pictureInPictureAdPtr = value;
+            }
+        }
+
         public void CreatePictureInPictureAd()
         {
-            // No-op for iOS dummy client.
+            if (this.PictureInPictureAdPtr != IntPtr.Zero)
+            {
+                return;
+            }
+            this._pictureInPictureAdClientPtr = (IntPtr)GCHandle.Alloc(this);
+            this.PictureInPictureAdPtr =
+                Externs.GADUCreatePictureInPictureAd(
+                    this._pictureInPictureAdClientPtr);
+            Externs.GADUSetPictureInPictureAdCallbacks(
+                this.PictureInPictureAdPtr,
+                PictureInPictureAdLoadedCallback,
+                PictureInPictureAdFailedToLoadCallback,
+                PictureInPictureAdShownCallback,
+                PictureInPictureAdHiddenCallback,
+                PictureInPictureAdDidRecordImpressionCallback,
+                PictureInPictureAdDidRecordClickCallback,
+                PictureInPictureAdDidFailToShowCallback,
+                PictureInPictureAdWillPresentFullScreenContentCallback,
+                PictureInPictureAdDidDismissFullScreenContentCallback,
+                PictureInPictureAdPaidEventCallback);
         }
 
         public void LoadAd(string adUnitId, AdRequest request)
         {
-            // No-op for iOS dummy client.
+            if (this.PictureInPictureAdPtr == IntPtr.Zero)
+            {
+                CreatePictureInPictureAd();
+            }
+            IntPtr requestPtr = Utils.BuildAdRequest(request);
+            Externs.GADULoadPictureInPictureAd(
+                this.PictureInPictureAdPtr, adUnitId, requestPtr);
+            Externs.GADURelease(requestPtr);
         }
 
         public void Show(PictureInPictureAdPosition position)
         {
-            // No-op for iOS dummy client.
+            Externs.GADUShowPictureInPictureAd(
+                this.PictureInPictureAdPtr, (int)position);
         }
 
         public void Hide()
         {
-            // No-op for iOS dummy client.
+            Externs.GADUHidePictureInPictureAd(this.PictureInPictureAdPtr);
         }
 
         public void Destroy()
         {
-            // No-op for iOS dummy client.
+            Externs.GADUDestroyPictureInPictureAd(this.PictureInPictureAdPtr);
+            this.PictureInPictureAdPtr = IntPtr.Zero;
+        }
+
+        public string GetAdUnitID()
+        {
+            return Externs.GADUGetPictureInPictureAdUnitID(
+                this.PictureInPictureAdPtr);
         }
 
         public PictureInPictureAdPosition GetAdPosition()
         {
-            return PictureInPictureAdPosition.Default;
+            return (PictureInPictureAdPosition)
+                Externs.GADUGetPictureInPictureAdPosition(
+                    this.PictureInPictureAdPtr);
         }
 
         public IResponseInfoClient GetResponseInfoClient()
         {
-            return null;
+            return new ResponseInfoClient(
+                ResponseInfoClientType.AdLoaded, this.PictureInPictureAdPtr);
         }
+
+        public void Dispose()
+        {
+            this.Destroy();
+            if (this._pictureInPictureAdClientPtr == IntPtr.Zero)
+            {
+                return;
+            }
+            ((GCHandle)this._pictureInPictureAdClientPtr).Free();
+            this._pictureInPictureAdClientPtr = IntPtr.Zero;
+        }
+
+        ~PictureInPictureAdClient()
+        {
+            this.Dispose();
+        }
+
+        #region Picture-in-Picture ad callback methods
+
+        [MonoPInvokeCallback(typeof(GADUPictureInPictureAdLoadedCallback))]
+        private static void PictureInPictureAdLoadedCallback(
+            IntPtr pipAdClient)
+        {
+            PictureInPictureAdClient client =
+                IntPtrToPictureInPictureAdClient(pipAdClient);
+            if (client != null && client.OnAdLoaded != null)
+            {
+                client.OnAdLoaded.Invoke();
+            }
+        }
+
+        [MonoPInvokeCallback(typeof(GADUPictureInPictureAdFailToLoadCallback))]
+        private static void PictureInPictureAdFailedToLoadCallback(
+            IntPtr pipAdClient, IntPtr error)
+        {
+            PictureInPictureAdClient client =
+                IntPtrToPictureInPictureAdClient(pipAdClient);
+            if (client != null && client.OnAdFailedToLoad != null)
+            {
+                LoadAdErrorClientEventArgs args =
+                    new LoadAdErrorClientEventArgs()
+                {
+                    LoadAdErrorClient = new LoadAdErrorClient(error),
+                };
+                client.OnAdFailedToLoad.Invoke(args);
+            }
+        }
+
+        [MonoPInvokeCallback(typeof(GADUPictureInPictureAdShownCallback))]
+        private static void PictureInPictureAdShownCallback(
+            IntPtr pipAdClient)
+        {
+            PictureInPictureAdClient client =
+                IntPtrToPictureInPictureAdClient(pipAdClient);
+            if (client != null && client.OnAdShown != null)
+            {
+                client.OnAdShown.Invoke();
+            }
+        }
+
+        [MonoPInvokeCallback(typeof(GADUPictureInPictureAdHiddenCallback))]
+        private static void PictureInPictureAdHiddenCallback(
+            IntPtr pipAdClient)
+        {
+            PictureInPictureAdClient client =
+                IntPtrToPictureInPictureAdClient(pipAdClient);
+            if (client != null && client.OnAdHidden != null)
+            {
+                client.OnAdHidden.Invoke();
+            }
+        }
+
+        [MonoPInvokeCallback(
+            typeof(GADUPictureInPictureAdDidRecordImpressionCallback))]
+        private static void PictureInPictureAdDidRecordImpressionCallback(
+            IntPtr pipAdClient)
+        {
+            PictureInPictureAdClient client =
+                IntPtrToPictureInPictureAdClient(pipAdClient);
+            if (client != null && client.OnAdDidRecordImpression != null)
+            {
+                client.OnAdDidRecordImpression.Invoke();
+            }
+        }
+
+        [MonoPInvokeCallback(
+            typeof(GADUPictureInPictureAdDidRecordClickCallback))]
+        private static void PictureInPictureAdDidRecordClickCallback(
+            IntPtr pipAdClient)
+        {
+            PictureInPictureAdClient client =
+                IntPtrToPictureInPictureAdClient(pipAdClient);
+            if (client != null && client.OnAdClicked != null)
+            {
+                client.OnAdClicked.Invoke();
+            }
+        }
+
+        [MonoPInvokeCallback(typeof(
+            GADUPictureInPictureAdDidFailToShowCallback))]
+        private static void
+            PictureInPictureAdDidFailToShowCallback(
+                IntPtr pipAdClient, IntPtr error)
+        {
+            PictureInPictureAdClient client =
+                IntPtrToPictureInPictureAdClient(pipAdClient);
+            if (client != null &&
+                client.OnAdFailedToPresentFullScreenContent != null)
+            {
+                AdErrorClientEventArgs args = new AdErrorClientEventArgs()
+                {
+                    AdErrorClient = new AdErrorClient(error),
+                };
+                client.OnAdFailedToPresentFullScreenContent.Invoke(args);
+            }
+        }
+
+        [MonoPInvokeCallback(typeof(
+            GADUPictureInPictureAdWillPresentFullScreenContentCallback))]
+        private static void
+            PictureInPictureAdWillPresentFullScreenContentCallback(
+                IntPtr pipAdClient)
+        {
+            PictureInPictureAdClient client =
+                IntPtrToPictureInPictureAdClient(pipAdClient);
+            if (client != null &&
+                client.OnAdDidPresentFullScreenContent != null)
+            {
+                client.OnAdDidPresentFullScreenContent.Invoke();
+            }
+        }
+
+        [MonoPInvokeCallback(typeof(
+            GADUPictureInPictureAdDidDismissFullScreenContentCallback))]
+        private static void
+            PictureInPictureAdDidDismissFullScreenContentCallback(
+                IntPtr pipAdClient)
+        {
+            PictureInPictureAdClient client =
+                IntPtrToPictureInPictureAdClient(pipAdClient);
+            if (client != null &&
+                client.OnAdDidDismissFullScreenContent != null)
+            {
+                client.OnAdDidDismissFullScreenContent.Invoke();
+            }
+        }
+
+        [MonoPInvokeCallback(typeof(GADUPictureInPictureAdPaidEventCallback))]
+        private static void PictureInPictureAdPaidEventCallback(
+            IntPtr pipAdClient, int precision, long value,
+            string currencyCode)
+        {
+            PictureInPictureAdClient client =
+                IntPtrToPictureInPictureAdClient(pipAdClient);
+            if (client != null && client.OnPaidEvent != null)
+            {
+                AdValue adValue = new AdValue()
+                {
+                    Precision = (AdValue.PrecisionType)precision,
+                    Value = value,
+                    CurrencyCode = currencyCode
+                };
+                client.OnPaidEvent.Invoke(adValue);
+            }
+        }
+
+        private static PictureInPictureAdClient
+            IntPtrToPictureInPictureAdClient(IntPtr pipAdClient)
+        {
+            GCHandle handle = (GCHandle)pipAdClient;
+            return handle.Target as PictureInPictureAdClient;
+        }
+
+        #endregion
     }
 }
+#endif
